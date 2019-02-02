8081658ce64e8918f8949f92e9be2ba3f496fa99
antsuma <anton.nordgren1@gmail.com>
2019-02-02 18:25:30 +0100

    asd

--- server.js
+++ server.js
@@ -5,10 +5,9 @@ const app = express();
 const port = process.env.port || 9001;
 
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile('index.html');
 });
 
 console.log('Listening on port', port);
