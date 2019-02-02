293c004ff29e32e965720f1df45891d44d921d6c
antsuma <anton.nordgren1@gmail.com>
2019-02-02 18:33:07 +0100

    asd

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.port || 9001;
+const port = process.env.PORT || 9001;
 
 app.use(express.static(__dirname));
 
